c05aec85b731b391f8fbaf097ff08a0e66856f1d
BSerg89 <barkin.s.m@gmail.com>
2024-05-15 01:49:57 +0700

    add count for win and count for lose + img win lose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import sys
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -15,25 +17,69 @@ target_width = 100
 target_height = 100
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-# print(target_x)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-# print(target_y)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-# color = (255, 255, 255)
+
+you_win_img = pygame.image.load("Images/you_win.png")
+you_win_width = 100
+you_win_height = 100
+
+you_win_x = SCREEN_WIDTH / 2 - you_win_width / 2
+you_win_y = SCREEN_HEIGHT / 2 - you_win_height / 2
+
+you_lose_img = pygame.image.load("Images/you_lose.png")
+you_lose_width = 100
+you_lose_height = 100
+
+you_lose_x = SCREEN_WIDTH / 2 - you_lose_width / 2
+you_lose_y = SCREEN_HEIGHT / 2 - you_lose_height / 2
+
+count_target = 0
+count_target_win = 5
+count_not_target = 0
+count_target_lose = 5
 
 running = True
+winner = False
+game_over = False
 while running:
     screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                count_target += 1
+                print(f"В цель {count_target}")
+                if count_target == count_target_win:
+                    winner = True
+                    start_time = pygame.time.get_ticks()
+            else:
+                count_not_target += 1
+                print(f"Мимо {count_not_target}")
+                if count_not_target == count_target_lose:
+                    game_over = True
+                    start_time = pygame.time.get_ticks()
 
-    screen.blit(target_img, (target_x, target_y))
+    if winner:
+        screen.fill(color)
+        screen.blit(you_win_img, (you_win_x, you_win_y))
+    elif game_over:
+        screen.fill(color)
+        screen.blit(you_lose_img, (you_lose_x, you_lose_y))
+    else:
+        screen.blit(target_img, (target_x, target_y))
+
     pygame.display.update()
 
+    if winner or game_over:
+        current_time = pygame.time.get_ticks()
+        if current_time - start_time >= 3000:  
+            running = False
+
 pygame.quit()
+sys.exit()
